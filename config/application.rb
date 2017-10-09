a0f4b1a596d91705fe74ef165bda2875a1141f29
anapaulalg <ana_paula_lg@hotmail.com>
2017-10-09 16:05:51 +0200

    Create app

---
+++ config/application.rb
@@ -0,0 +1,17 @@
+require_relative 'boot'
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(*Rails.groups)
+
+module Evaluations
+  class Application < Rails::Application
+    config.load_defaults 5.1
+
+    console do
+      ActiveRecord::Base.connection
+    end
+  end
+end
