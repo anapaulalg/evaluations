7fea3a8aeccbb321f0ca6c1f7b433bdf71ada07b
anapaulalg <ana_paula_lg@hotmail.com>
2017-10-10 15:06:27 +0200

    Edit, update and delete students

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -13,13 +13,44 @@ class StudentsController < ApplicationController
   end
 
   def create
-    student_params = params.require(:student).permit(:name, :image)
     @student = Student.new(student_params)
-
+    @student.batch = Batch.find(params[:batch_id])
     if @student.save
-       redirect_to @student
+       redirect_to @student.batch
     else
        render 'new'
     end
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    @student = Student.find(params[:id])
+
+    if @student.update_attributes(student_params)
+      redirect_to @student
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @batch = Batch.find(params[:batch_id])
+    @student = @batch.students.find(params[:id])
+    @student.destroy
+
+    redirect_to @batch
+  end
+
+  private
+
+  def set_student
+    @student = Student.find(params[:id])
+  end
+
+  def student_params
+    params.require(:student).permit(:name, :image)
   end
 end
