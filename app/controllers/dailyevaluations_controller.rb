7ef65482ab6b9852a36afd519e1dc80c50601d0a
anapaulalg <ana_paula_lg@hotmail.com>
2017-10-12 19:12:35 +0200

    Update edit evaluation

--- app/controllers/dailyevaluations_controller.rb
+++ app/controllers/dailyevaluations_controller.rb
@@ -8,4 +8,41 @@ class DailyevaluationsController < ApplicationController
     @dailyevaluation = Dailyevaluation.find(params[:id])
   end
 
+  def new
+    @dailyevaluation = Dailyevaluation.new
+  end
+
+  def create
+    @dailyevaluation = Dailyevaluation.new(dailyevaluation_params)
+    @dailyevaluation.student = Student.find(params[:student_id])
+    if @dailyevaluation.save
+       redirect_to @dailyevaluation.student
+    else
+       render 'new'
+    end
+  end
+
+  def edit
+    @dailyevaluation = Dailyevaluation.find(params[:id])
+  end
+
+  def update
+    @dailyevaluation = Dailyevaluation.find(params[:id])
+
+    if @dailyevaluation.update_attributes(dailyevaluation_params)
+      redirect_to batch_student_path(@dailyevaluation.student.batch.id, @dailyevaluation.student.id)
+    else
+      render 'edit'
+    end
+  end
+
+  private
+
+  def set_dailyevaluation
+    @dailyevaluation = Dailyevaluation.find(params[:id])
+  end
+
+  def dailyevaluation_params
+    params.require(:dailyevaluation).permit(:evaluation, :date, :remark)
+  end
 end
