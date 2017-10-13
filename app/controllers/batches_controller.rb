b4816592e65a0cf1e63a71e5bd16b1dd87466e93
anapaulalg <ana_paula_lg@hotmail.com>
2017-10-13 10:08:13 +0200

    Add model and view for random ask question

--- app/controllers/batches_controller.rb
+++ app/controllers/batches_controller.rb
@@ -1,5 +1,4 @@
 class BatchesController < ApplicationController
-
   def index
     @batches = Batch.all
   end
@@ -13,23 +12,24 @@ class BatchesController < ApplicationController
 
     total_students = @batch.students.length
 
-    @batch.students.each do |student|
-      last_evaluation = student.dailyevaluations.order(:date).last
-      if last_evaluation
-        if last_evaluation.evaluation == "excellent"
-          excellent_total += 1
-        elsif last_evaluation.evaluation == "good"
-          good_total += 1
-        else
-          bad_total += 1
+    if (total_students > 0)
+      @batch.students.each do |student|
+        last_evaluation = student.dailyevaluations.order(:date).last
+        if last_evaluation
+          if last_evaluation.evaluation == "excellent"
+            excellent_total += 1
+          elsif last_evaluation.evaluation == "good"
+            good_total += 1
+          else
+            bad_total += 1
+          end
         end
       end
+
+      @excellent = (excellent_total * 100) / total_students
+      @good = (good_total * 100) / total_students
+      @bad = (bad_total * 100) / total_students
     end
-
-    @excellent = (excellent_total * 100) / total_students
-    @good = (good_total * 100) / total_students
-    @bad = (bad_total * 100) / total_students
-
   end
 
   def new
@@ -40,9 +40,21 @@ class BatchesController < ApplicationController
     batch_params = params.require(:batch).permit(:name, :start, :end)
     @batch = Batch.new(batch_params)
     if @batch.save
-       redirect_to @batch
+       redirect_to @batch, notice: "Batch successfully saved!"
     else
-       render 'new'
+       render 'new', notice: "Error while saving batch!"
     end
+  end
+
+  def ask_question
+    @batch = Batch.find(params[:batch_id])
+    @student = @batch.random_student()
+    @last_evaluation = @student.dailyevaluations.order(:date).last
+
+    if (@student.nil?)
+      @batch.students.sample
+    end
+
+    render 'askquestion'
   end
 end
